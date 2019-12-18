9ae94c759ea90161705e6a0adbd7d821ebe1c82c
Ashish Patel <toaashishpatel@gmail.com>
2019-12-19 00:02:08 +0530

    added demo project - change port numbers

---
+++ src/AzureKeyVaultLabs.Demo/Program.cs
@@ -0,0 +1,22 @@
+#region Imports
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+#endregion
+
+namespace AzureKeyVaultLabs.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/AzureKeyVaultLabs.Demo/Startup.cs
@@ -0,0 +1,35 @@
+#region Imports
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+#endregion
+
+namespace AzureKeyVaultLabs.Demo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
