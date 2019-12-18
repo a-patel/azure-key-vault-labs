d11bbff12a9ee4e4dfb6a8b598bb0bff80cd98ea
Ashish Patel <toaashishpatel@gmail.com>
2019-12-19 02:05:02 +0530

    Connect to Azure Key Vault using the Client Id and Client Secret (AAD). - NuGets: Added required nugets - KeyVault.Core and Ext.Config.AzureKV - AppSettings: Added required settings - Added Reference links

--- src/AzureKeyVaultLabs.Demo/Program.cs
+++ src/AzureKeyVaultLabs.Demo/Program.cs
@@ -1,5 +1,7 @@
 #region Imports
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 #endregion
 
@@ -14,9 +16,41 @@ namespace AzureKeyVaultLabs.Demo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var settings = config.Build();
+
+                    var keyVaultEndpoint = settings["AzureKeyVault:Endpoint"];
+                    var keyVaultClientId = settings["AzureKeyVault:ClientId"];
+                    var keyVaultClientSecret = settings["AzureKeyVault:ClientSecret"];
+
+                    // Way-1
+                    // Connect to Azure Key Vault using the Client Id and Client Secret (AAD).
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint) && !string.IsNullOrEmpty(keyVaultClientId) && !string.IsNullOrEmpty(keyVaultClientSecret))
+                    {
+                        config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClientId, keyVaultClientSecret, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
     }
 }
+
+
+
+#region Reference
+
+/*
+https://github.com/Azure-Samples/key-vault-dotnet-core-quickstart
+https://medium.com/@dneimke/add-keyvault-to-an-asp-net-core-application-cab1012d2b60
+https://wakeupandcode.com/key-vault-for-asp-net-core-web-apps/
+http://anthonygiretti.com/2019/04/29/configuring-azure-keyvault-in-asp-net-core-2-2-and-azure/
+
+// AAD
+https://docs.microsoft.com/en-us/azure/media-services/previous/media-services-portal-get-started-with-aad
+
+*/
+
+#endregion
